495af6b1f6f32813da41ee133b8e62594e990f57
vicktor349 <ayomidevictor349@gmail.com>
2022-12-25 17:01:27 +0100

    Added Custom Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,22 @@ module.exports = {
       },
       screens: {
         'xsm': { 'max': '480px' },
-      }
+      },
+      margin: {
+        '520px': '520px',
+        '1000px': '1000px',
+        '1400px': '1400px'
+      },
+      height: {
+        '350px': '350px',
+        '2109px': '2109px',
+        '1452px': '1452px'
+      },
+      width: {
+        '350px': '350px',
+        '524px': '524px',
+        '444px': '444px'
+      },
     },
 
   },
